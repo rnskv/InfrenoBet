defb1574b4dfa661873bc0f30b72f0b308bc549c
rnskv <web.rnskv@gmail.com>
2020-02-04 20:40:47 +0300

    Add supporting .env for client

--- client/src/modules/realtime/index.js
+++ client/src/modules/realtime/index.js
@@ -1,5 +1,6 @@
 import Socket from 'src/modules/sockets';
 
+console.log('123', process.env)
 export const ws = new Socket({
     url: 'http://localhost:3000',
 });
--- client/webpack.dev.config.js
+++ client/webpack.dev.config.js
@@ -1,43 +1,57 @@
+const webpack = require('webpack');
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
 
-module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            src: path.resolve(__dirname, 'src'),
-            ui: path.resolve(__dirname, 'src/ui'),
+module.exports = () => {
+    const env = dotenv.config().parsed;
+    const envKeys = Object.keys(env).reduce((prev, next) => {
+        prev[`${next}`] = JSON.stringify(env[next]);
+        return prev;
+    }, {});
+
+    return ({
+        entry: './src/index.jsx',
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'index_bundle.js',
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js|jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
+        resolve: {
+            extensions: ['.js', '.jsx'],
+            alias: {
+                src: path.resolve(__dirname, 'src'),
+                ui: path.resolve(__dirname, 'src/ui'),
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js|jsx$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    },
                 },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': envKeys
+            }),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, '/src/index.html'),
+            }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, '/src/index.html'),
-        }),
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        historyApiFallback: true,
-        port: 9000,
-    },
-};
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            historyApiFallback: true,
+            port: 9000,
+        },
+    });
+}
