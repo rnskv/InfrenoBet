c41cd5c915c55771a3e6c5b8fce47cd2c348fb9b
rnskv <web.rnskv@gmail.com>
2020-03-22 23:32:02 +0300

    Fix adorable size

--- backend/src/controllers/auth/actions.js
+++ backend/src/controllers/auth/actions.js
@@ -30,7 +30,7 @@ const registerHandler = async (ctx) => {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
 
-    await new User({ email, name, password: hash, avatar: `https://api.adorable.io/avatars/12/${hash}`}).save();
+    await new User({ email, name, password: hash, avatar: `https://api.adorable.io/avatars/70/${email}`}).save();
 
     ctx.body = {
         ok: true
