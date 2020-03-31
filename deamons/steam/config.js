1937ce70102769df91268b7261a775a9ec78ab12
rnskv <web.rnskv@gmail.com>
2020-03-31 21:55:22 +0300

    Split bot config to prod and dev

--- deamons/steam/config.js
+++ deamons/steam/config.js
@@ -1,8 +1,22 @@
 module.exports = {
-    LOGIN: 'xtrell322',
-    PASSWORD: 'Ntktw20021656',
-    SHARED_SECRET: 'EYJzPPtrYCLs5la5c8ru6avZBn4=',
-    DOMAIN: 'infernobet.ru',
-    IDENTITY_SECRET: 'xiQFxcTGiYNmfO8HzPYx2Gy5CIk=',
-    API_URL: 'http://backend:6001',
+    development: {
+        LOGIN: 'xtrell322',
+        PASSWORD: 'Ntktw20021656',
+        SHARED_SECRET: 'EYJzPPtrYCLs5la5c8ru6avZBn4=',
+        IDENTITY_SECRET: 'xiQFxcTGiYNmfO8HzPYx2Gy5CIk=',
+        DOMAIN: 'infernobet.ru',
+        API_URL: 'http://127.0.0.2:6001',
+        REDIS_HOST: '127.0.0.1',
+        REDIS_PORT: '6379',
+    },
+    production: {
+        LOGIN: 'xtrell322',
+        PASSWORD: 'Ntktw20021656',
+        SHARED_SECRET: 'EYJzPPtrYCLs5la5c8ru6avZBn4=',
+        IDENTITY_SECRET: 'xiQFxcTGiYNmfO8HzPYx2Gy5CIk=',
+        DOMAIN: 'infernobet.ru',
+        API_URL: 'http://backend:6001',
+        REDIS_HOST: 'redis',
+        REDIS_PORT: '6379',
+    }
 };
