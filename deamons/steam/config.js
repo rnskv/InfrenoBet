3ffcaeebb62e429ab919860d5b0201f626aedbf9
rnskv <web.rnskv@gmail.com>
2020-03-31 21:10:21 +0300

    Fixes

--- deamons/steam/config.js
+++ deamons/steam/config.js
@@ -4,5 +4,5 @@ module.exports = {
     SHARED_SECRET: 'EYJzPPtrYCLs5la5c8ru6avZBn4=',
     DOMAIN: 'infernobet.ru',
     IDENTITY_SECRET: 'xiQFxcTGiYNmfO8HzPYx2Gy5CIk=',
-    API_URL: 'http://127.0.0.1:6001',
+    API_URL: 'http://backend:6001',
 };
