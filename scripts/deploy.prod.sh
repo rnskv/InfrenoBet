9f3ee666c5a89ea199d0cb6bec0165b5151cfefe
rnskv <web.rnskv@gmail.com>
2020-04-13 21:06:12 +0300

    Update deploy script

--- scripts/deploy.prod.sh
+++ scripts/deploy.prod.sh
@@ -1,12 +1,14 @@
-CWD="$PWD/scripts"
-
-bash $CWD/docker-compose_push.sh
-
+SCRIPTS_DIRECTORY="$PWD/scripts"
+PROJECT_DIRECTORY='$PWD/inferno';
 USER="root"
 SSH="ssh -i $PWD/deploy_key $USER:$SERVER_IP_ADDRESS"
 URL="$SERVER_IP_ADDRESS:$USER"
+COPY_DOCKER_COMPOSE="scp -i $PWD/deploy_key $PWD/docker-compose.yml $USER:$SERVER_IP_ADDRESS:~/inferno"
+bash $SCRIPTS_DIRECTORY/docker-compose_push.sh
+
 echo "URL => $URL"
 
-DEPLOY="docker-compose pull && docker-compose stop && docker-compose up"
+DEPLOY="cd ~inferno && docker-compose pull && docker-compose stop && docker-compose up"
 
+$COPY_DOCKER_COMPOSE
 $SSH $DEPLOY
