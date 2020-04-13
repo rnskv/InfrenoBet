56c2aa2b0b8aeadc5f739f4871f8cc9cb0bcd65e
rnskv <web.rnskv@gmail.com>
2020-04-13 21:43:32 +0300

    Fix root user

--- scripts/deploy.prod.sh
+++ scripts/deploy.prod.sh
@@ -1,9 +1,9 @@
 SCRIPTS_DIRECTORY="$PWD/scripts"
 PROJECT_DIRECTORY="$PWD/inferno";
 USER="root"
-SSH="ssh -i $PWD/deploy_key $SERVER_IP_ADDRESS:$USER"
-URL="$SERVER_IP_ADDRESS:$USER"
-COPY_DOCKER_COMPOSE="scp -i $PWD/deploy_key $PWD/docker-compose.yml $SERVER_IP_ADDRESS:$USER:~/inferno"
+SSH="ssh -i $PWD/deploy_key $SERVER_IP_ADDRESS@$USER"
+URL="$SERVER_IP_ADDRESS@$USER"
+COPY_DOCKER_COMPOSE="scp -i $PWD/deploy_key $PWD/docker-compose.yml $SERVER_IP_ADDRESS@$USER:~/inferno"
 DEPLOY="cd ~inferno && docker-compose pull && docker-compose stop && docker-compose up"
 
 bash $SCRIPTS_DIRECTORY/docker-compose_push.sh
