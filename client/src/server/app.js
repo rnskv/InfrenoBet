430ed33588c62146a6bd50d15b117f035b46064c
rnskv <web.rnskv@gmail.com>
2020-05-10 23:05:54 +0300

    Add yandex verification

--- client/src/server/app.js
+++ client/src/server/app.js
@@ -65,8 +65,8 @@ function renderFullPage(html, preloadedState, styleTags) {
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <meta name="viewport" content="width=1100px, initial-scale=0.3">
             <title>INFERNOBET - играй и побеждай!</title>
-            <meta name=description content="Сервис, в котором игроки DOTA 2 и обычные пользователи могут испытать свою удачу в захватывающей игре и получить уникальные скины!">
-            <meta name=keywords content="рулетка, бомж рулетка, ставки дота2, лотерея на деньги, рулетка дота 2, скины дота 2, магазин скинов дота 2, лотерея дота, быстрые скины дота 2, дабл ставки, быстрые скины дота 2, быстрые ставки, рулетка dota2, скины dota 2, лотерея dota, розыгрыши dota"/>
+            <meta name=description content="Сервис, в котором каждый может испытать свою удачу в захватывающей игре и получить уникальные скины!">
+            <meta name=keywords content="infernobet, рулетка, бомж рулетка, ставки дота2, лотерея на деньги, рулетка дота 2, скины дота 2, магазин скинов дота 2, лотерея, дота, быстрые скины дота 2, дабл ставки, быстрые скины дота 2, быстрые ставки, рулетка dota2, скины dota 2, лотерея dota, розыгрыши dota"/>
             <link rel="shortcut icon" href="/dist/resources/images/favicon.png" type="image/png">
             <link rel="canonical" href="https://infernobet.ru/"/>
             <meta name="verification" content="cf0bb3d99b3a1dba77e550d078a322" />
