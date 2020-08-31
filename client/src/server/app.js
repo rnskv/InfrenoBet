fc2766620096d6b1b84a4c5e0c4e857d98d4dc86
Roman Nuiskov <web.rnskv@gmail.com>
2020-08-31 19:46:07 +0300

    Add yandex metrika

--- client/src/server/app.js
+++ client/src/server/app.js
@@ -73,6 +73,21 @@ function renderFullPage(html, preloadedState, styleTags) {
             <meta name="swiftpay-verification" content="bf4a552b8fef2160a0b001ca4b7f11e3" />
             <meta name="yandex-verification" content="266251ef2bca23cc" />
             <script data-ad-client="ca-pub-4020574099528906" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- Yandex.Metrika counter -->
+            <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(66907300, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+            });
+            </script>
+            <noscript><div><img src="https://mc.yandex.ru/watch/66907300" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            <!-- /Yandex.Metrika counter -->
             ${styleTags}
         </head>
         <body>
